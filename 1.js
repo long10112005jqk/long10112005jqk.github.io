8cdd7a0d50a3ab8d3423a03cea7b0c702a04eaf0
long <hailongavatar1@gmail.com>
2021-05-24 11:26:55 +0700

    them nua vao

--- 1.js
+++ 1.js
@@ -1,4 +1,8 @@
 ﻿ $(function(){
- 
+ 	$(window).scroll(function() {
+ 		if ($('body').scrollTop() > 100) {
+ 		$('.navbar.navbar-light.bg-faded.navbar-fixed-top').addClass('maunen')
+ 		}
+ 	})
 })  
  
