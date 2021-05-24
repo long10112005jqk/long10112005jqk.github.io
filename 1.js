f69390217864a48aac18ab57c1a7f8191aae1eb4
long <hailongavatar1@gmail.com>
2021-05-24 13:37:33 +0700

    chinh sua

--- 1.js
+++ 1.js
@@ -1,8 +1,31 @@
 ﻿ $(function(){
  	$(window).scroll(function() {
- 		if ($('body').scrollTop() > 100) {
- 		$('.navbar.navbar-light.bg-faded.navbar-fixed-top').addClass('maunen')
+ 		if ($('html').scrollTop() > 400)
+ 		{
+ 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').addClass('maunen');
+ 		} else
+ 		{
+ 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').removeClass('maunen');
  		}
+ 		if ($('html').scrollTop() > 100)
+ 		{
+ 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').addClass('maunenmobile');
+ 		} else
+ 		{
+ 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').removeClass('maunenmobile');
+ 		}
+ 		if($('html').scrollTop() > 400)
+ 		{
+ 			$('a.navbar-brand.pull-right.nutlen').addClass('maunenduoi');
+ 		} else
+ 		{
+ 			$('a.navbar-brand.pull-right.nutlen').removeClass('maunenduoi');
+ 		}
+ 	})
+ 	
+ 	$('a.navbar-brand.pull-right.nutlen').click(function() {
+ 		$('html').animate({'scrollTop':0});
+ 		return false;
  	})
 })  
  
