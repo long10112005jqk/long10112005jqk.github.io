6f9bbdecc623ea20a98f327abb2389d6b58d689a
long <hailongavatar1@gmail.com>
2021-06-02 18:54:08 +0700

    thay doi gg

--- 1.js
+++ 1.js
@@ -1,58 +1,41 @@
-﻿ $(function(){
- 	$(window).scroll(function() {
- 		if ($('html').scrollTop() > 400)
- 		{
- 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').addClass('maunen');
- 		} else
- 		{
- 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').removeClass('maunen');
- 		}
- 		if ($('html').scrollTop() > 100)
- 		{
- 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').addClass('maunenmobile');
- 		} else
- 		{
- 			$('.navbar.navbar-light.bg-faded.navbar-fixed-top').removeClass('maunenmobile');
- 		}
- 		if($('html').scrollTop() > 400)
- 		{
- 			$('a.navbar-brand.pull-right.nutlen').addClass('maunenduoi');
- 		} else
- 		{
- 			$('a.navbar-brand.pull-right.nutlen').removeClass('maunenduoi');
- 		}
- 	})
- 	
- 	$('a.navbar-brand.pull-right.nutlen').click(function() {
- 		$('html').animate({'scrollTop':0});
- 		return false;
- 	})
+﻿document.addEventListener("DOMContentLoaded",function(){
+    var menu = document.querySelector(".menu"),
+        vongquay = document.querySelector(".vongquay");
+        muc1 = document.querySelector(".muc1");
+        muc2 = document.querySelector(".muc2");
+        muc3 = document.querySelector(".muc3");
+        nut = document.querySelector(".nut");
+        menuan = document.querySelector(".menuan");
+        nenden = document.querySelector(".nenden");
+    window.addEventListener("scroll",function(){
 
-}) 
-document.addEventListener("DOMContentLoaded",function() {
-	var nutan = document.getElementById("nutan"),
-		menu = document.getElementsByClassName("menu"),
-		nenden = document.getElementsByClassName("nenden"),
-		khoito = document.getElementsByClassName("khoito"),
-		tt = false;
-	var menu = menu[0],
-		nenden = nenden[0],
-		khoito	 = khoito[0];
-	nutan.onclick = function() {
-		menu.classList.add("dayra");
-		nenden.classList.add("hienden");
-		khoito.classList.add("nhovao");
-		tt = true;
-	}
-	nenden.onclick = function() {
-		if (tt = true) 
-		{
-			menu.classList.remove("dayra");
-			nenden.classList.remove("hienden");
-			khoito.classList.remove("nhovao");
-			tt = false;
-		}
-	}
-	console.log(document.cookie);
+        if (window.pageYOffset > vongquay.offsetTop) 
+        {
+            menu.classList.add("nenmenu");
+        }
+        else
+        {
+            menu.classList.remove("nenmenu");
+        }
+        if (window.pageYOffset > muc1.offsetTop-400) 
+        {
+            muc1.classList.add("cdmuc1");
+        }
+        if (window.pageYOffset > muc1.offsetTop+300) 
+        {
+            muc2.classList.add("cdmuc2");
+        }
+         if (window.pageYOffset > muc1.offsetTop+1000) 
+        {
+            muc3.classList.add("cdmuc3");
+        }
+    })
+    nut.addEventListener("click",function(){
+        nenden.classList.add("hiennenden");
+        menuan.classList.add("hienmenuan");
+    })
+    nenden.addEventListener("click",function() {
+        nenden.classList.remove("hiennenden");
+        menuan.classList.remove("hienmenuan");
+    })
 },false)
- 
